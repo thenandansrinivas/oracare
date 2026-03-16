f999548ff3a7fdec313d1463ead05cdd7c63b414
Nandan S <thenandansrinivas@outlook.com>
2026-03-16 21:59:16 +0530

    Initial Commit - Monorepo Setup - Done

---
+++ web/src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+	return <div className='m-5 p-10 text-3xl'>{import.meta.env.VITE_SERVER_PORT}</div>
+}
+
+export default App
---
+++ web/vite.config.ts
@@ -0,0 +1,16 @@
+import path from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+	plugins: [react(), tailwindcss()],
+	envDir: '../',
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			'@server': path.resolve(__dirname, '../server/src')
+		}
+	}
+})
